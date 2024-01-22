a744ffbd6f1f3312d44143325556d3864b48a284
AKarelina <annagjindj@gmail.com>
2024-01-22 08:17:43 -0800

    Add removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from "./Todolist";
 
@@ -8,28 +8,43 @@ const App = () => {
     const todolistTitle_2: string = "What to do"
     const todolistTitle_3: string = "What to review"
 
-    let tasks1: TaskType[] = [
+const [tasks, setTasks] = useState([
         {id: 1, title: "CSS", isDone: true},
         {id: 2, title: "JS", isDone: false},
         {id: 3, title: "React", isDone: false}
-    ]
+                                         ])
 
-    let tasks2: TaskType[] = [
-        {id: 4, title: "CV", isDone: true},
-        {id: 5, title: "Exam", isDone: false},
-        {id: 6, title: "Set up environment", isDone: false}
-    ]
+    // let tasks1: TaskType[] = [
+    //     {id: 1, title: "CSS", isDone: true},
+    //     {id: 2, title: "JS", isDone: false},
+    //     {id: 3, title: "React", isDone: false}
+    // ]
+    //
+    // let tasks2: TaskType[] = [
+    //     {id: 4, title: "CV", isDone: true},
+    //     {id: 5, title: "Exam", isDone: false},
+    //     {id: 6, title: "Set up environment", isDone: false}
+    // ]
+    //
+    // let tasks3: TaskType[] = [
+    //     {id: 7, title: "Questions", isDone: true},
+    //     {id: 8, title: "React", isDone: true},
+    //     {id: 9, title: "JS", isDone: false}
+    // ]
 
-    let tasks3: TaskType[] = [
-        {id: 7, title: "Questions", isDone: true},
-        {id: 8, title: "React", isDone: true},
-        {id: 9, title: "JS", isDone: false}
-    ]
+    const removeTask = (taskId: number) => {
+        const newTasksState: Array<TaskType> = []
+        for(let i = 0; i < tasks.length; i++) {
+            if(tasks[i].id !== taskId){
+                newTasksState.push(tasks[i])
+            }
+        } setTasks(newTasksState)
+    }
     return (
         <div className="App">
-            <Todolist title={todolistTitle_1} tasks={tasks1}/>
-            <Todolist title={todolistTitle_2} tasks={tasks2}/>
-            <Todolist title={todolistTitle_3} tasks={tasks3}/>
+            <Todolist removeTask={removeTask} title={todolistTitle_1} tasks={tasks}/>
+            {/*<Todolist removeTask={removeTask} title={todolistTitle_2} tasks={tasks2}/>*/}
+            {/*<Todolist removeTask={removeTask} title={todolistTitle_3} tasks={tasks3}/>*/}
         </div>
     );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -4,43 +4,57 @@ import React from "react";
 export type TaskType = {
     title: string,
     id: number,
-    isDone: boolean
+    isDone: boolean,
+
 }
 
 
 type PropsType = {
     title: string,
-    tasks: TaskType[]
+    tasks: TaskType[],
+    removeTask: (taskId: number) => void
 }
-export const Todolist: React.FC<PropsType> = ({title, tasks}) => {
-    // const {title, tasks} = props
-    return (
-        <div className="todolist">
-            <h3>{title}</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                <li>
-                    <input type="checkbox" checked={tasks[0].isDone}/>
-                    <span>{tasks[0].title}</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={tasks[1].isDone}/>
-                    <span>{tasks[1].title}</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={tasks[2].isDone}/>
-                    <span>{tasks[2].title}</span>
-                </li>
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
-        </div>
-    )
+
+
+export const Todolist: React.FC<PropsType> = (props) => {
+    let listItems: Array<JSX.Element> = props.tasks.map(t => {
+        const onClickRemoveTaskHandler = () => props.removeTask(t.id)
+        return (
+            <li
+                key={t.id}>
+                <input type="checkbox" checked={t.isDone}/>
+                <span>{t.title}</span>
+                <button onClick={onClickRemoveTaskHandler}>X</button>
+            </li>)
+    })
+    //if the list is not empty than show listitems
+    const tasksList: JSX.Element = props.tasks.length
+        ? <ul>{listItems}</ul>
+        : <span>Your tasks list is empty</span>
+
+return (<div className="todolist">
+    <h3>{props.title}</h3>
+    <div>
+        <input/>
+        <button>+</button>
+    </div>
+    {tasksList}
+    {/*<ul>*/}
+    {/*    {props.tasks.map(t => {*/}
+    {/*    return (*/}
+    {/*        <li key={t.id}>*/}
+    {/*            <input type="checkbox" checked={t.isDone}/>*/}
+    {/*            <span>{t.title}</span>*/}
+    {/*        </li>)*/}
+    {/*    })}*/}
+    {/*{listItems}*/}
+    {/*</ul>*/}
+
+    <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+    </div>
+</div>)
 }
 
